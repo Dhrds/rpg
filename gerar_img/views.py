da3dd3968dbcf23a6bd87941fef10119a81a0e1e
Dhrds <dhrds1996@gmail.com>
2024-03-28 20:58:00 -0300

    gerar imagem ok

--- gerar_img/views.py
+++ gerar_img/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import HttpResponse, redirect
-from openai import OpenAI
+import openai
 import os
+from django.shortcuts import render
 
 api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+client = openai.api_key = api_key
 
 
 def gera_img(request):
 
     if request.method == 'POST':
         texto = request.POST.get('promt')
-        response = client.images.generate(
+        response = openai.Image.create(
             model="dall-e-3",
             prompt=texto,
             size="1024x1024",
@@ -20,22 +21,4 @@ def gera_img(request):
 
         image_url = response.data[0].url
         return redirect(image_url)
-    return HttpResponse('''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Formulário Django</title>
-    </head>
-    <body>
-        <h1>Formulário Django</h1>
-        <form method="post" action="{% url 'processar_formulario' %}">
-            {% csrf_token %}
-            <label for="nome">promt:</label><br>
-            <input type="text" id="promt" name="promt"><br>
-            <input type="submit" value="Enviar">
-        </form>
-    </body>
-    </html>
-                        ''')
+    return render(request, 'gerar.html')
